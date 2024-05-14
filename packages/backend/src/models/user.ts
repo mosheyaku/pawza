4f43b12108ac944a3b6c5486b66ef4a4fa6aa4d7
JasonMan34 <JasonErlMan@gmail.com>
2024-05-14 16:12:09 +0300

    Create user model (#1)

--- packages/backend/src/models/user.ts
+++ packages/backend/src/models/user.ts
@@ -1,5 +1,17 @@
 import mongoose, { type InferSchemaType } from 'mongoose';
 
+export enum UserPurpose {
+  Platonic = 'platonic',
+  Romantic = 'romantic',
+  All = 'all',
+}
+
+export enum Gender {
+  Man = 'man',
+  Woman = 'woman',
+  Other = 'other',
+}
+
 const userSchema = new mongoose.Schema(
   {
     email: {
@@ -8,6 +20,22 @@ const userSchema = new mongoose.Schema(
       index: true,
       unique: true,
       lowercase: true,
+    },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    password: { type: String, required: true },
+    birthDate: { type: Date, required: true },
+    gender: { type: String, required: true, enum: Object.values(Gender) },
+    purpose: { type: String, required: true, enum: Object.values(UserPurpose) },
+    // From the docs - https://mongoosejs.com/docs/geojson.html
+    location: {
+      type: { type: String, enum: ['Point'], required: true },
+      coordinates: { type: [Number], required: true },
+    },
+    // Array of urls
+    photos: {
+      type: [String],
+      required: true,
     },
   },
   { timestamps: true },
