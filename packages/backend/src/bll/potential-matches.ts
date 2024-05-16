01e93de5ebf847c3dd05e859e7007d5219e18cfe
JasonMan34 <JasonErlMan@gmail.com>
2024-05-16 16:48:14 +0300

    getPotentialMatches return 5 random

--- packages/backend/src/bll/potential-matches.ts
+++ packages/backend/src/bll/potential-matches.ts
@@ -4,7 +4,7 @@ import { UserModel } from '../models/user.js';
 
 export const getPotentialMatches = async (userId: mongoose.Types.ObjectId | string) => {
   // TODO: Implement this
-  const matches = await UserModel.find().limit(5);
+  const matches = await UserModel.aggregate([{ $sample: { size: 5 } }]);
 
   return matches;
 };
