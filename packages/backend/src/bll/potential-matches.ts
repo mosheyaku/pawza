6751e03d0d748fb3d6798efc78f1520b069b3ac8
JasonMan34 <JasonErlMan@gmail.com>
2024-05-21 00:05:47 +0300

    add comment for Gal :)

--- packages/backend/src/bll/potential-matches.ts
+++ packages/backend/src/bll/potential-matches.ts
@@ -3,7 +3,10 @@ import type mongoose from 'mongoose';
 import { UserModel } from '../models/user.js';
 
 export const getPotentialMatches = async (userId: mongoose.Types.ObjectId | string) => {
-  // TODO: Implement this
+  // TODO: GAL
+  // MUST HAVE FOR WEDNESDAY:
+  // change this so that if there is a PotentialMatch of status ACCEPTED, that user will NOT be suggested
+
   const matches = await UserModel.aggregate([{ $sample: { size: 5 } }]);
 
   return matches;
