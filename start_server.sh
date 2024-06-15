50e35d668f7baa78564823e53373ea29e5548449
JasonMan34 <JasonErlMan@gmail.com>
2024-06-16 02:33:55 +0300

    -9

--- start_server.sh
+++ start_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd packages/backend
-ps aux | grep node | grep -v grep | awk '{print $2}' | xargs kill
+ps aux | grep node | grep -v grep | awk '{print $2}' | xargs kill -9
 sleep 2
 nohup pnpm run prod &
