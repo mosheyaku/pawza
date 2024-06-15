06019037f16c7f27e2bc6137aa865056467e90c3
JasonMan34 <JasonErlMan@gmail.com>
2024-06-15 18:55:28 +0300

    add back err msg for sh scrilp

--- start_server.sh
+++ start_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd packages/backend
-kill $(cat pid) 2> /dev/null || true  # Ignore errors if the process is not running
+kill $(cat pid) || true  # Ignore errors if the process is not running
 nohup pnpm run prod &
 echo $! > pid
