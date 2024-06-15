b21ab9db7894894d43840e04b2d41d1fa832a7ec
JasonMan34 <JasonErlMan@gmail.com>
2024-06-15 22:00:54 +0300

    build version

---
+++ packages/backend/src/public/assets/chats-Fe-X9ueK.js
@@ -0,0 +1,1 @@
+import{a9 as a}from"./index-BL5xB9rp.js";const c=()=>a.get("/chats").then(s=>s.data),n=s=>a.get(`/chats/${s}/messages`).then(t=>t.data),o=(s,t)=>a.post(`/chats/${s}/messages`,{content:t}).then(e=>e.data),g=s=>a.get(`/chats/${s}`).then(t=>t.data);export{g as a,n as b,c as f,o as s};
