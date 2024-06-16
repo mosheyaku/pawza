764f12b30855d25e1e25fad4fad8a0e164df7ed6
JasonMan34 <JasonErlMan@gmail.com>
2024-06-16 18:23:49 +0300

    version

--- packages/backend/src/public/assets/chats-Dg2olIt2.js
+++
@@ -1,1 +0,0 @@
-import{a9 as a}from"./index-C31y_Pqt.js";const c=()=>a.get("/chats").then(s=>s.data),n=s=>a.get(`/chats/${s}/messages`).then(t=>t.data),o=(s,t)=>a.post(`/chats/${s}/messages`,{content:t}).then(e=>e.data),g=s=>a.get(`/chats/${s}`).then(t=>t.data);export{g as a,n as b,c as f,o as s};
