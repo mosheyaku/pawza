85309ce1922f58366b70d5779650034d2f3bb237
JasonMan34 <JasonErlMan@gmail.com>
2024-06-18 16:05:03 +0300

    Invalid credentials pop up if there is any error

--- packages/frontend/src/features/LoginPage/index.tsx
+++ packages/frontend/src/features/LoginPage/index.tsx
@@ -1,4 +1,5 @@
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { Alert, CircularProgress, Snackbar } from '@mui/material';
 import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
@@ -13,7 +14,6 @@ import { type ChangeEvent, type FormEvent, useState } from 'react';
 import { setApiClientTokens } from '../../api/base';
 import { login as loginApiCall } from '../../api/login';
 import { useAuth } from '../Auth/useAuth';
-import FullScreenLoader from '../Loader/FullScreenLoader';
 
 export default function LoginPage() {
   const { rive, RiveComponent } = useRive({
@@ -26,22 +26,13 @@ export default function LoginPage() {
   const handsUp = useStateMachineInput(rive, 'StateMachine1', 'hands_up');
   const check = useStateMachineInput(rive, 'StateMachine1', 'Check');
   const look = useStateMachineInput(rive, 'StateMachine1', 'Look');
-
-  const animationHandle = () => {
-    check!.value = false;
-  };
-
-  const checkHandle = (e: ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
-    handsUp!.value = false;
-    check!.value = true;
-    look!.value = email.length * 3;
-  };
+  const success = useStateMachineInput(rive, 'StateMachine1', 'success');
 
   const { setUser, user } = useAuth();
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [openFailed, setOpenFailed] = useState(false);
 
   const {
     mutateAsync: login,
@@ -51,19 +42,39 @@ export default function LoginPage() {
     mutationFn: (data: { email: string; password: string }) => loginApiCall(data),
   });
 
+  const animationHandle = () => {
+    if (check) {
+      check.value = true;
+    }
+  };
+
+  const checkHandle = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+
+    if (handsUp && check && look) {
+      handsUp.value = false;
+      check.value = true;
+      look.value = email.length * 3;
+    }
+  };
+
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     try {
       const res = await login({ email, password });
       if (res.status === 200) {
+        if (success) {
+          success.fire();
+        }
+
         const { user: newUser, token, refreshToken } = res.data;
 
         setUser(newUser);
         setApiClientTokens(token, refreshToken);
       }
     } catch (e) {
-      // Empty catch, it's ok for now
+      setOpenFailed(true);
     }
   };
 
@@ -76,57 +87,62 @@ export default function LoginPage() {
   // TODO: If error, handle it (show message or something)
   return (
     <Container maxWidth="xs">
-      {showLoader ? (
-        <FullScreenLoader />
-      ) : (
-        <Box display="flex" flexDirection="column" alignItems="center">
-          <Box sx={{ width: '100%', height: 200 }}>
-            <RiveComponent />
-          </Box>
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Log in
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              label="Email"
-              autoComplete="email"
-              autoFocus
-              value={email}
-              onChange={checkHandle}
-              onClick={() => (handsUp!.value = false)}
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              label="Password"
-              type="password"
-              autoComplete="current-password"
-              sx={{ mt: 0 }}
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              onSelect={() => (handsUp!.value = true)}
-            />
-            <Button
-              type="submit"
-              onClick={() => animationHandle()}
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2, bgcolor: 'secondary.main' }}
-            >
-              Log in
-            </Button>
-
-            <Link to="/signup">{"Don't have an account? Sign Up"}</Link>
-          </Box>
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Box sx={{ width: '100%', height: 200 }}>
+          <RiveComponent />
         </Box>
-      )}
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          {showLoader ? <CircularProgress size="50%" /> : <LockOutlinedIcon />}
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Log in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            label="Email"
+            autoComplete="email"
+            autoFocus
+            value={email}
+            onChange={checkHandle}
+            onClick={() => (handsUp!.value = false)}
+            disabled={showLoader}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+            sx={{ mt: 0 }}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            onSelect={() => (handsUp!.value = true)}
+            disabled={showLoader}
+          />
+          <Button
+            type="submit"
+            onClick={() => animationHandle()}
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2, bgcolor: 'secondary.main' }}
+            disabled={showLoader}
+          >
+            Log in
+          </Button>
+
+          <Link to="/signup">{"Don't have an account? Sign Up"}</Link>
+        </Box>
+      </Box>
+
+      <Snackbar open={openFailed}>
+        <Alert severity="error" onClose={() => setOpenFailed(false)}>
+          Invalid credentials
+        </Alert>
+      </Snackbar>
     </Container>
   );
 }
